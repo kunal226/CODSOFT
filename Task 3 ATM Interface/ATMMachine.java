2ae68dbae0121e2a4337ea2a54634a809e73313a
Kunal <129434545+kunal226@users.noreply.github.com>
2024-05-21 13:53:26 +0530

    Add files via upload

---
+++ Task 3 ATM Interface/ATMMachine.java
@@ -0,0 +1,28 @@
+public class ATMMachine implements ATMInterface {
+    public static long availablemoney;
+
+    public ATMMachine(int availablemoney) {
+        ATMMachine.availablemoney = availablemoney;
+    }
+
+    @Override
+    public void withdrawAmount(long amount) {
+        if (availablemoney > amount) {
+            System.out.println("Transaction Succeeded of :- ₹" + amount);
+            availablemoney -= amount;
+        } else {
+            System.out.println("Insufficient Funds! So couldn't complete the transaction. Sorry for the inconvinience!!!!");
+        }
+    }
+
+    @Override
+    public void depositAmount(long amount) {
+        System.out.println("Money Deposited In Bank of :- ₹" + amount);
+        availablemoney += amount;
+    }
+
+    @Override
+    public void checkBalance() {
+        System.out.println("The Money Available in your account is :- ₹" + availablemoney);
+    }
+}
